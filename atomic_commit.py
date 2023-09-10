1c1ef0f8cbbd3208258cbe21eb15b7e635b9983f
jamesbrown <randomvoidmail@foxmail.com>
2023-09-10 18:52:32 +0800

    update

--- atomic_commit.py
+++ atomic_commit.py
@@ -1,7 +1,37 @@
 import os
+import sys
+
+def get_script_path_and_exec_cmd(script_prefix):
+    """
+    Get the script path and the command to execute the script.
+
+    Args:
+        script_prefix (str): The prefix of the script name.
+
+    Returns:
+        tuple: A tuple containing the script path (str) and the command to execute the script (str).
+    """
+    script_path = f"{script_prefix}.py"
+    exec_prefix = sys.executable
+    if not os.path.exists(script_path):
+        if os.name == "nt":
+            script_suffix = "cmd"
+            exec_prefix = "cmd /C"
+        else:
+            script_suffix = "sh"
+            exec_prefix = "bash"
+        script_path = f"{script_prefix}.{script_suffix}"
+        assert os.path.exists(
+            script_path
+        ), f"failed to find os native implementation of commit script: {script_path}"
+
+    cmd = f"{exec_prefix} {script_path}"
+    return script_path, cmd
+
+
 # deadlock: if both backup integrity & fsck failed, what to do?
 # when backup is done, put head hash as marker
-#                                                      / n nothing - commit      
+#                                                      / n nothing - commit
 # fsck - succ -          d_comm > d_back? - y backup  /
 #      - fail - rollback /
 
