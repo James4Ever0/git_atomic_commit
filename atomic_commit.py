5b36005d3eb2bc2f975af764664d247fd927dbdd
jamesbrown <randomvoidmail@foxmail.com>
2023-09-10 23:54:34 +0800

    update

--- atomic_commit.py
+++ atomic_commit.py
@@ -1,10 +1,108 @@
 import os
 import sys
 
-REQUIRED_BINARIES = ['bash', 'timemachine', 'rsync']
+# import shutil
+
+# instead of 'where', we have `shutil.which`
+
+
+def _access_check(fn, mode):
+    return os.path.exists(fn) and os.access(fn, mode) and not os.path.isdir(fn)
+
+
+from typing import Iterator
+
+
+def where(cmd, mode=os.F_OK | os.X_OK, path=None) -> Iterator[str]:
+    """Given a command, mode, and a PATH string, generate all paths which
+    conforms to the given mode on the PATH.
+
+    `mode` defaults to os.F_OK | os.X_OK. `path` defaults to the result
+    of os.environ.get("PATH"), or can be overridden with a custom search
+    path.
+
+    """
+    # If we're given a path with a directory part, look it up directly rather
+    # than referring to PATH directories. This includes checking relative to the
+    # current directory, e.g. ./script
+    if os.path.dirname(cmd):
+        if _access_check(cmd, mode):
+            return cmd
+        return None
+
+    use_bytes = isinstance(cmd, bytes)
+
+    if path is None:
+        path = os.environ.get("PATH", None)
+        if path is None:
+            try:
+                path = os.confstr("CS_PATH")
+            except (AttributeError, ValueError):
+                # os.confstr() or CS_PATH is not available
+                path = os.defpath
+        # bpo-35755: Don't use os.defpath if the PATH environment variable is
+        # set to an empty string
+
+    # PATH='' doesn't match, whereas PATH=':' looks in the current directory
+    if not path:
+        return None
+
+    if use_bytes:
+        path = os.fsencode(path)
+        path = path.split(os.fsencode(os.pathsep))
+    else:
+        path = os.fsdecode(path)
+        path = path.split(os.pathsep)
+
+    if sys.platform == "win32":
+        # The current directory takes precedence on Windows.
+        curdir = os.curdir
+        if use_bytes:
+            curdir = os.fsencode(curdir)
+        if curdir not in path:
+            path.insert(0, curdir)
+
+        # PATHEXT is necessary to check on Windows.
+        pathext_source = os.getenv("PATHEXT") or _WIN_DEFAULT_PATHEXT
+        pathext = [ext for ext in pathext_source.split(os.pathsep) if ext]
+
+        if use_bytes:
+            pathext = [os.fsencode(ext) for ext in pathext]
+        # See if the given file matches any of the expected path extensions.
+        # This will allow us to short circuit when given "python.exe".
+        # If it does match, only test that one, otherwise we have to try
+        # others.
+        if any(cmd.lower().endswith(ext.lower()) for ext in pathext):
+            files = [cmd]
+        else:
+            files = [cmd + ext for ext in pathext]
+    else:
+        # On other platforms you don't have things like PATHEXT to tell you
+        # what file suffixes are executable, so just pass on cmd as-is.
+        files = [cmd]
+
+    seen = set()
+    for dir in path:
+        normdir = os.path.normcase(dir)
+        if not normdir in seen:
+            seen.add(normdir)
+            for thefile in files:
+                name = os.path.join(dir, thefile)
+                if _access_check(name, mode):
+                    yield name
+
+
+# for path in where('git'): # multiple git now.
+#   print(path)
+
+
+REQUIRED_BINARIES = ["bash", "timemachine", "rsync", "git"]
+# if on windows, we check if bash is not coming from wsl.
+FSCK = "git fsck"
 
 # you may find usable bash shell next to our git executable on windows, and it is preferred
 # because wsl bash sucks
+
 
 def get_script_path_and_exec_cmd(script_prefix):
     """
@@ -36,9 +134,6 @@ def get_script_path_and_exec_cmd(script_prefix):
 
 # deadlock: if both backup integrity & fsck failed, what to do?
 # when backup is done, put head hash as marker
-#                                                      / n nothing - commit
-# fsck - succ -          d_comm > d_back? - y backup  /
-#      - fail - rollback /
 
 r"""
 atomic backup:
@@ -54,7 +149,7 @@ r"""
    /      \
  succ     fail
      \     | rollback (most recent backup)
- d_comm > d_back? # can be replaced by metadata check
+ d_comm > d_back? # can be replaced by metadata check or `git rev-ref HEAD` alongside metadata check with depth=1 (if possible to retrieve from timemachine current backup)
     | y     |n
   backup (atomic) & update d_back nothing
     \       /
@@ -62,7 +157,7 @@ r"""
         |
       fsck
    succ/  \fail
-      |    |rollback (most recent backup) & exit
+      |    | rollback (most recent backup) & exit
    update d_comm
     | backup (atomic)
     | update d_back
